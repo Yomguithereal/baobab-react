7efea34f8ec221fb6581be3bb6bf7f100b9c1a23
Yomguithereal <guillaumeplique@gmail.com>
2016-03-04 14:58:20 +0100

    Cooking curry

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -4,6 +4,19 @@
  *
  * Miscellaneous helper functions.
  */
+function curry(fn, arity, acc) {
+  acc = acc || [];
+
+  return function() {
+    var args = Array.prototype.slice.call(arguments);
+
+    if (args.length < arity)
+      return curry(fn, arity - 1, acc.concat(args));
+
+    return fn.apply(null, args);
+  };
+}
+
 function solveMapping(mapping, props, context) {
   if (typeof mapping === 'function')
     mapping = mapping(props, context);
