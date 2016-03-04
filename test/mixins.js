b3dc4ea5c872f7f2c54023619986a6ef351537d5
Yomguithereal <guillaumeplique@gmail.com>
2016-03-04 16:12:35 +0100

    Starting unit tests

--- test/mixins.js
+++ test/mixins.js
@@ -3,3 +3,37 @@
  * ===============================
  *
  */
+import assert from 'assert';
+import React from 'react';
+import {mount, shallow} from 'enzyme';
+import * as mixins from '../src/mixins';
+
+/**
+ * Components.
+ */
+const DummyRoot = React.createClass({
+  mixins: [mixins.root],
+  render() {
+    return <div />;
+  }
+});
+
+const Root = React.createClass({
+  mixins: [mixins.root],
+  render() {
+    return <div>{this.props.children}</div>;
+  }
+});
+
+/**
+ * Test suite.
+ */
+describe('Mixins', function() {
+
+    it('should fail if passing a wrong tree to the root mixin.', function() {
+
+      assert.throws(function() {
+        mount(<DummyRoot tree={{hello: 'world'}} />);
+      }, /Baobab/);
+    });
+});
