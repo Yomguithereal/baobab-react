d998bc9278530c9cb95e7a915bc34e1e64ce5527
Joseph Clay <joe@weareimmutable.com>
2019-04-08 11:10:15 -0500

    expose hooks

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
   higherOrder: require('./higher-order.js'),
   mixins: require('./mixins.js'),
+  hooks: require('./hooks.js'),
   PropTypes: require('./dist-modules/utils/prop-types.js').default,
 };
